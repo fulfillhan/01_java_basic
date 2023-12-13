f45519a07e58dbcc20ae4bc1df01278c3eed3085
15_web_hsh <15_web_hsh@DESKTOP-HHECRK8>
2023-12-13 17:45:16 +0900

    2023-12-13 커밋

--- 01_java_basic/src/step1_06/loop/LoopEx28_문제.java
+++ 01_java_basic/src/step1_06/loop/LoopEx28_문제.java
@@ -48,12 +48,20 @@ public class LoopEx28_문제 {
 		 * ###
 		 */
 		
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j <i ; j++) {
-	
-			}
-			System.out.println("#");
+//		for (int i = 0; i < 3; i++) {
+//			for (int j = 0; j < 3-(i+1); j++) {
+//				System.out.println(" ");
+//			}
+//			for (int j = 0; j <= i; j++) {
+//				System.out.println("#");
+//			}
+//		}
+		int i=0;
+		while(i < 3) {
 			
+			for (int j = 0; j < ; j++) {
+				
+			}
 		}
 		
 		
