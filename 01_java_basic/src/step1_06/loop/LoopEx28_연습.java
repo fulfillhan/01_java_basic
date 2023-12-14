283e39efde2f21e1a27c8c2fbfcc6547bb546f42
sanghui <tkdgml719@gamil.com>
2023-12-14 12:03:12 +0900

    2023-12-14 오전커밋

--- 01_java_basic/src/step1_06/loop/LoopEx28_연습.java
+++ 01_java_basic/src/step1_06/loop/LoopEx28_연습.java
@@ -1,9 +1,10 @@
 package step1_06.loop;
 
+import java.util.Iterator;
 
 // # 삼각형 그리기
 
-public class LoopEx28_문제 {
+public class LoopEx28_연습 {
 	
 	public static void main(String[] args) {
 		
@@ -48,21 +49,18 @@ public class LoopEx28_문제 {
 		 * ###
 		 */
 		
-//		for (int i = 0; i < 3; i++) {
-//			for (int j = 0; j < 3-(i+1); j++) {
-//				System.out.println(" ");
-//			}
-//			for (int j = 0; j <= i; j++) {
-//				System.out.println("#");
-//			}
-//		}
-		int i=0;
-		while(i < 3) {
-			
-			for (int j = 0; j < ; j++) {
-				
+		for (int i = 0; i < 3; i++) { // 행의 갯수 3
+			for (int j = 0; j < 2-i; j++) { // 행의 반복하면서 2회 ""출력한다.
+				System.out.print(" ");
 			}
+			for (int j = 0; j <= i; j++) {// "#"의 갯수 :  j=0 i=0 1개/ j=1 i=1 2개/ j=2 i=2 3개
+				System.out.print("#");
+			}
+			System.out.println();
 		}
+	
+		System.out.println();
+
 		
 		
 		/*
@@ -71,6 +69,16 @@ public class LoopEx28_문제 {
 		 * ##
 		 * #
 		 */
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3-i; j++) { // 00,11,22
+				System.out.print("#");
+			}
+			for (int j = 0; j< i; j++) {//0,1,2,
+				System.out.print(" ");
+			}
+			System.out.println();
+			
+		}
 
 		
 		/*
